99dba43d85ab8890a753d392f44c65d2372f2b38
dbeure <45558507+dbeure@users.noreply.github.com>
2019-04-30 12:39:43 +0200

    Update const.py
    for second try of the new model (with bigger dataset)

--- romanesco/const.py
+++ romanesco/const.py
@@ -14,7 +14,7 @@ VOCAB_FILENAME = 'vocab.json'
 CONFIG_FILENAME = 'config.json'
 
 VOCAB_SIZE = 10000
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 
 NUM_EPOCHS = 13
 
@@ -23,7 +23,7 @@ NUM_EPOCHS = 13
 NUM_STEPS = 100 # truncated backprop length
 LEARNING_RATE = 0.001
 
-HIDDEN_SIZE = 1024  # RNN state size
+HIDDEN_SIZE = 512  # RNN state size
 EMBEDDING_SIZE = 256  # embedding vector size
 
 SAMPLE_LENGTH = 100
